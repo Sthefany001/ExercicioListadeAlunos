6a50e12e8ea8979c33477fa8a5c46af7957d03f8
Sthefany <s532401ed@gmail.com>
2023-07-31 17:23:26 -0400

    first commit

---
+++ 04 - Exercício Lista Aluno/Aluno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04___Exercício_Lista_Aluno
+{
+    internal class Aluno
+    {
+        public int Matricula { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+
+        public DateOnly DataNascimento { get; set; }
+
+        public Aluno()
+        {
+
+        }
+
+        public Aluno(int matricula, string nome, string cpf, DateOnly dataNascimento)
+        {
+            this.Matricula = matricula;
+            this.Nome = nome;
+            this.Cpf = cpf;
+            this.DataNascimento = dataNascimento;
+        }
+    }
+}
---
+++ 04 - Exercício Lista Aluno/Program.cs
@@ -0,0 +1,44 @@
+﻿using _04___Exercício_Lista_Aluno;
+
+List<Aluno> alunos = new List<Aluno>();
+
+while (true)
+{
+    Aluno x = new Aluno();
+    Console.WriteLine("Digite a matrícula do aluno(a): ");
+    x.Matricula = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Digite o nome do aluno(a): ");
+    x.Nome = Console.ReadLine();
+    Console.WriteLine("Digite o cpf do aluno(a): ");
+    x.Cpf = Console.ReadLine();
+    Console.WriteLine("Digite a data de nascimento do aluno(a): ");
+    x.DataNascimento = DateOnly.Parse(Console.ReadLine());
+
+    alunos.Add(x);
+
+    Console.WriteLine("Deseja cadastrar um aluno? ");
+    Console.WriteLine("0 - Não");
+    Console.WriteLine("1 - Sim");
+    Console.Write("Opção escolhida: ");
+    int opcao = Convert.ToInt32(Console.ReadLine());
+
+    if(opcao == 0)
+    {
+        break;
+    }
+    else if(opcao == 1)
+    {
+
+    }
+    else
+    {
+        Console.WriteLine("Opção Inválida!");
+    }
+}
+
+////
+
+foreach (Aluno a in alunos)
+{
+    Console.WriteLine($"\n O nome é {a.Nome} {a.Cpf} ");
+}
