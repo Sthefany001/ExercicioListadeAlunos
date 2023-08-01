e31eb4168b8419980f764ef904fe3041d874d170
Sthefany <s532401ed@gmail.com>
2023-08-01 13:16:57 -0400

    third commit

--- 04 - Exercício Lista Aluno/Program.cs
+++ 04 - Exercício Lista Aluno/Program.cs
@@ -5,18 +5,18 @@ List<Aluno> alunos = new List<Aluno>();
 while (true)
 {
     Aluno x = new Aluno();
-    Console.WriteLine("Digite a matrícula do aluno(a): ");
+    Console.Write("\nDigite a matrícula do aluno(a): ");
     x.Matricula = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Digite o nome do aluno(a): ");
+    Console.Write("Digite o nome do aluno(a): ");
     x.Nome = Console.ReadLine();
-    Console.WriteLine("Digite o cpf do aluno(a): ");
+    Console.Write("Digite o cpf do aluno(a): ");
     x.Cpf = Console.ReadLine();
-    Console.WriteLine("Digite a data de nascimento do aluno(a): ");
+    Console.Write("Digite a data de nascimento do aluno(a): ");
     x.DataNascimento = DateOnly.Parse(Console.ReadLine());
 
     alunos.Add(x);
 
-    Console.WriteLine("Deseja cadastrar um aluno? ");
+    Console.WriteLine("\nDeseja cadastrar um aluno? ");
     Console.WriteLine("0 - Não");
     Console.WriteLine("1 - Sim");
     Console.Write("Opção escolhida: ");
@@ -40,5 +40,8 @@ while (true)
 
 foreach (Aluno a in alunos)
 {
-    Console.WriteLine($"\n O nome é {a.Nome} {a.Cpf} ");
+    Console.WriteLine($"\n\n MATRÍCULA: {a.Matricula}");
+    Console.WriteLine($" NOME: {a.Nome}");
+    Console.WriteLine($" CPF: {a.Cpf}");
+    Console.WriteLine($" DATA NASCIMENTO: {a.DataNascimento}");
 }
